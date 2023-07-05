5e263a66518bf770e3c888883a9c8fbe1d545e4d
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-05 16:10:59 +0200

    end basic exercise

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      emails: []
+    }
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res)=> {
+          this.emails.push(res.data.response)
+        })
     }
   }
 }).mount('#app')
+
