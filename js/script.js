d64e9a3fc5f51df2429313233546f630c610825d
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-05 18:11:47 +0200

    add loader and  end bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /*
   - Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+  // soluzione di marco 
+
   vue stampa gli elementi in base al cambio del data , quindi se pushiamo direttamente le email che vengono da axios nell'array emails vue farà uscire le email cosi come arrivano , 
   per prevenire questo comportamento dobbiamo inserire tutti i dati con un solo push , 
 
@@ -18,9 +20,9 @@ createApp({
     for (let i = 0; i < 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((res)=> {
-          this.emails.push(res.data.response)
+          this.emails.push(res.data.response);
         })
     }
   }
-}).mount('#app')
+}).mount('#app');
 
