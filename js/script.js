2e2c51d8b7a3aa12e016ff2694a666d21b30defb
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-05 16:23:02 +0200

    created basic reasoning for bonus

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+/*
+  - Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+  vue stampa gli elementi in base al cambio del data , quindi se pushiamo direttamente le email che vengono da axios nell'array emails vue farà uscire le email cosi come arrivano , 
+  per prevenire questo comportamento dobbiamo inserire tutti i dati con un solo push , 
+
+  quindi possiamo creare un secondo array all'interno di created dove pusheremo i dati che vengono da axios e poi (usando lo spred) li inseriamo nell'array in data , cosi che l'inserimento sia fatto con un solo push e i dati vengano caricati tutti in una volta 
+*/
 const { createApp } = Vue
 
 createApp({
